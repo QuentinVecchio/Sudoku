e35893c4561f1705657818faf408126a28b13f0d
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-05-15 14:05:45 +0200

    d?but d'analyse Init_Data, ne compile pas encore

---
+++ initialisation.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+#include "GestionCandidats.h"
+#include "GestionCases.h"
+
+#include "initialisation.h"
+
+main()
+{
+	printf("hello world\n");
+}
+
+
+void Init_Data(L_Candidats * LC)
+{
+	//SPECIFICATION
+	
+	int i, j; //index de parcours du tableau 'LC'
+	
+	TabInit * Init; //tableau de valeur du fichier
+	int n; //index de parcours du tableau 'Init'
+	
+	
+	//CORPS
+	
+	
+	/*
+	 * on ferme les cases qui sont dans le fichier txt
+	 * et on leur attribut NULL 
+	 */
+	
+	/*
+	 * on ouvre les cases qui ne sont pas dans le fichier txt
+	 * on attribut a la case un pointeur sur la liste des candidats de la case
+	 */
+	
+	//Initialisation da la variable Init avec la fonctio lireFic
+	*Init = lireFic(&n);
+	
+	
+	
+	for(i=0; i<9; i++)
+	{
+		for(j=0; j<9; j++)
+		{
+			if(i == Init(n).ligne && j== Init(n))
+			{
+				LC[i][j] = NULL;
+				n++;
+			}
+			else
+			{
+				//traitement pour ajouter la liste des candidats
+			}
+		}
+	}
+}
+
+
+/*
+ * une fonction qui retourne un tableau de valeur correspondant au fichier txt fourni
+ * 
+ * @return un tableau de type TabInit
+ */
+*TabInit lireFic(int *n)
+{
+	n=0;
+	return NULL
+}
---
+++ initialisation.h
@@ -0,0 +1,8 @@
+typedef struct TabInit
+{
+	int ligne;
+	int colone;
+}TabInit;
+
+
+void Init_Data(L_Candidats LC, L_Case LO)
