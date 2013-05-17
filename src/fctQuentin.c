2bb36636584c7e94696aad42914162b850a7ceef
Quentin Vecchio <vecchioquentin@hotmail.fr>
2013-05-17 13:06:33 +0200

    Modification de la fonction Est_Candidat

--- src/fctQuentin.c
+++ src/fctQuentin.c
@@ -2,9 +2,41 @@
 * Fonction pour projet C Quentin Vecchio
 * Fonction Est_Candidat et Fermer_Case
 */
-int Est_Candidat(int chiffre,T_Case Case , L_Candidats LC[][])
+int Est_Candidat(int chiffre,T_Case Case , int grille[][])
 {
-	return (appartient_liste(LC[Case.ligne][Case.Colonne],chiffre));
+/*
+	Fonction qui prend en paramètre une case et qui cherche si le chiffre donné peut etre clé candidate pour la case.
+	On cherche si le chiffre est dans la ligne de la case, ensuite de la colonne et enfin dans le carré. On s'aide de la grille qui est remplie.
+	La fonction s'arrête directement si elle trouve dans la grille le chiffre. Si l'opération se déroule bien alors le chifffe peut être candidat de la case.
+*/
+	int i;
+	//On cherche le chiffre dans la ligne
+	for(i=0;i<9;i++)
+        {
+		if(grille[i][Case.colonne] == chiffre)
+			return 0;
+        }
+        //On cherche le chiffre dans la colonne
+        for(i=0;i<9;i++)
+        {
+                if(grille[Case.ligne][i] == chiffre)
+			return 0;
+        }
+	//On cherche dans le carré
+	x = Case.ligne-Case.ligne%3;
+        y = Case.colonne-Case.colonne%3;
+        while(x != x+3)
+        {
+        	while(y != y+3)
+                {
+                      	if(grille[x][y] == chiffre)
+                        	return 0;
+                }
+                y -=3;
+                x++;
+         }
+
+	return 1;
 }
 
 void Fermer_Case(int chiffre, int grille[][],T_Case Case, L_Candidats LC[][])
@@ -30,8 +62,8 @@ void Fermer_Case(int chiffre, int grille[][],T_Case Case, L_Candidats LC[][])
 			}
                 }
 		//Suppression dans le carré et dans la case
-		x = Case.colonne-Case.colonne%3;
-		y = Case.ligne-Case.ligne%3;
+		x = Case.ligne-Case.ligne%3;
+		y = Case.colonne-Case.colonne%3;
 		while(x != x+3)
 		{
 			while(y != y+3)
