97e8b4cd1fdbd52594c421f9292b4e073ee8266c
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-05-21 17:27:12 +0200

    ajout grille de test

---
+++ tui/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "../src/GestionCandidats.h"
+#include "../src/struc.c"
+main()
+{
+	printf("Bienvenue dans le logiciel de résolution de sudoku\n");
+	L_Candidats LC[9][9];
+}
