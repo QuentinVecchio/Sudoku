94564a55a92907105afa0e2b40480ab1b411a778
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-05-24 00:23:36 +0200

    quand mercurial fait des siennes !

---
+++ tui/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "../src/GestionCandidats.h"
+#include "../src/GestionCases.h"
+#include "../src/initialisation.h"
+#include "affiche.h"
+
+int main()
+{
+	printf("Bienvenue dans le logiciel de résolution de sudoku\n");
+	int Grille[9][9]={{0}};
+	L_Candidats  LC[9][9];
+	L_Cases LO = NULL;
+	char lien[] = "res/g1.txt";
+
+	Init_Data(LC, LO, Grille, lien);
+	Affiche_Grille(Grille);
+	//affiche_liste_case(LO);
+	return 0;
+}
