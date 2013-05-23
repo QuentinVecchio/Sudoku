40a06fb9ea96c14ca93456c04b458b7b4eceb743
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-05-23 12:20:27 +0200

    debuggage de 'main.c'

--- tui/main.c
+++ tui/main.c
@@ -2,7 +2,7 @@
 #include "../src/GestionCandidats.h"
 #include "../src/GestionCases.h"
 #include "affiche.h"
-#include "../src/initialisation.c"
+
 int main()
 {
 	printf("Bienvenue dans le logiciel de résolution de sudoku\n");
