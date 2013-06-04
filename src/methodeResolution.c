374d87d56713c2d330b4c3e198529252413139a1
Matthieu CLIN <16208312+clinm@users.noreply.github.com>
2013-06-04 23:21:09 +0200

    prise en compte demande prof (affiche nombre de grille possible)prise en compte demande prof (affiche nombre de grille possible)

--- src/methodeResolution.c
+++ src/methodeResolution.c
@@ -7,18 +7,21 @@
 
 #include "methodeResolution.h"
 #include "../tui/affiche.h"
+#include "time.h"
+#include <stdlib.h>
+#include <string.h>
 int fermerGrille(int grille[9][9], L_Cases LO, L_Candidats LC[9][9])
 {
 	Affiche_Grille(grille);
 	L_Cases parcours=LO->suivant;
 	int res_Admet_Unique;
 	int colonne;
-/*	for(colonne = 0; colonne < 9; colonne++)*/
 	
 
 	int a_modifie =1;
 	while (parcours!=parcours->suivant && a_modifie==1)
 	{
+		printf("Boucle\n");
 		a_modifie=0;
 		do
 		{
@@ -42,89 +45,54 @@ int fermerGrille(int grille[9][9], L_Cases LO, L_Candidats LC[9][9])
 	else return 1;
 }
 
+int nbr_solution = 0;
+int solution[9][9];
+
 int Backtrack(int grille[9][9], L_Cases LO, L_Candidats LC[9][9])
 {
 	L_Cases tmp = LO->suivant;
-	//Affiche_Grille(grille);
-	
-	int l = 0;
-	return resolve_ligne(grille, LO, tmp, LC, l);
-// 	for(l=0; l <1; l++)
-// 	{
-// 		printf("\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!LANCEMENT !!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-// 		if(resolve_ligne(grille, LO, tmp, LC, l) == 0)
-// 		{
-// 				printf("ERREUR !\n");
-// 				break;
-// 		}else{
-// 			tmp = donne_prochaine_ligne(tmp);
-// 		}
-// 		
-// 		
-// 	}
-	
-
-	//printf("valeur!!!: %d\n",appartient_liste(LC[0][0], 54));
+	clock_t a,b;
+	double c;
+	a = clock();
+	resoud(grille, LO, tmp, LC);
+	b = clock();
+	c = (double)(b-a)/CLOCKS_PER_SEC;
+	Affiche_Grille(solution);
+	printf("Temps écoulé :%2.5f\n", c);
+	return nbr_solution;
 
 }
 
-int resolve_ligne(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9], int ligne)
+resoud(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9])
 {
-// 	printf("Appel de fonction !\n");
-// 	Affiche_Grille(grille);
-	int l = ligne;
-	int colonne;
-
-// 	for(colonne = 0; colonne < 9; colonne++)
-// 	{
-// 		printf("###Affichage de la ligne %d colonne %d\n", l, colonne);
-// 		affiche_liste_Candidats(LC[l][colonne]);
-// 	}
-	if(L == origine) return 1;
-	else{
-// 		printf("Case %d %d \n",L->courant.ligne,L->courant.colonne);
+	if(L == origine) {
+		nbr_solution++;
+		if(nbr_solution == 1)
+		{
+			memcpy(solution, grille, sizeof solution);
+		}
+		return;
+	}else{
 		L_Candidats c = LC[L->courant.ligne][L->courant.colonne];
 		int res;
-// 		printf("Etude de:");
-// 		affiche_liste_Candidats(c);
-// 		printf("\n");
-
+		int valeur_courant;
 		do{
-// 			printf("boucle\n");
-			if(c == NULL) return 0;
-// 			printf("%d\n\n", c->courant);
-			int valeur_courant = c->courant;
+			if(c == NULL) return ;
+
+			valeur_courant = c->courant;
 			Fermer_Case(c->courant,grille, L->courant, LC);
-			res=resolve_ligne(grille,origine, (L->suivant), LC, ligne);
-// 			printf("Retour de resolve avec %d Case %d %d\n", res, L->courant.ligne,L->courant.colonne);
-			if(!res)
+			resoud(grille,origine, (L->suivant), LC);
+
+			Ouvrir_Case(valeur_courant,grille,L->courant, LC,origine);
+			
+			c = LC[L->courant.ligne][L->courant.colonne]; 
+			while(c->suivant && c->courant != valeur_courant) 
 			{
-
-				Ouvrir_Case(valeur_courant,grille,L->courant, LC,origine);
-				
-				c = LC[L->courant.ligne][L->courant.colonne]; 
-				while(c->suivant && c->courant != valeur_courant) 
-				{
-					c = c->suivant;
-				}
-// 				for(colonne = 0; colonne < 9; colonne++)
-// 				{
-// 					printf("###Affichage de la ligne %d colonne %d\n", l, colonne);
-// 					affiche_liste_Candidats(LC[l][colonne]);
-// 				}
-// 				printf("---------------------------------------------------------------------");
-// 				printf("---------------------------------------------------------------------");
-// 				printf("%d %d\n",valeur_courant, c->courant);
-
 				c = c->suivant;
-			}else{
-					//Fermer_Case
-// 					printf("€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€e€");
-					return res;
 			}
+			c = c->suivant;
 		}while(c);
-// 		printf("Retour fin \n");
-		return 0;
+
 	}
 }
 
--- tui/main.c
+++ tui/main.c
@@ -3,14 +3,13 @@
 #include "../src/GestionCases.h"
 #include "../src/initialisation.h"
 #include "affiche.h"
-
 int main()
 {
 	printf("Bienvenue dans le logiciel de résolution de sudoku\n");
 	int Grille[9][9]={{0}};
 	L_Candidats  LC[9][9]= {{NULL}};
 	L_Cases LO = creer_liste_vide();
-	char lien[] = "res/g9.txt";
+	char lien[] = "res/g2.txt";
 
 	Init_Data(LC, LO, Grille, lien);
 	int grille_simple=fermerGrille(Grille, LO, LC);
@@ -23,10 +22,12 @@ int main()
 	else
 	{
 		printf("Grille complexe\n");
+		Affiche_Grille(Grille);
 		printf("Lancement du backtracking\n");
 		int grille_possible=Backtrack(Grille,LO,LC);
-		if (grille_possible==1)Affiche_Grille(Grille);
-		else printf("Grille impossible\n");
+		if (grille_possible){
+			printf("Il y a %d grilles possibles\n", grille_possible);
+		}else printf("Grille impossible\n");
 	}
 
 	return 0;
