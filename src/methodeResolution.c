6df9b49a9a0e442c235bc988c014c70a564eff37
Quentin Vecchio <vecchioquentin@hotmail.fr>
2013-06-06 10:23:34 +0200

    remise ? niveau de methodeResolution.c

--- src/methodeResolution.c
+++ src/methodeResolution.c
@@ -12,38 +12,37 @@
 #include <string.h>
 int fermerGrille(int grille[9][9], L_Cases LO, L_Candidats LC[9][9])
 {
-	//Affiche_Grille(grille);
-	L_Cases parcours=LO->suivant;
-	int res_Admet_Unique;
-	int colonne;
-/*	for(colonne = 0; colonne < 9; colonne++)*/
-	
+        Affiche_Grille(grille);
+        L_Cases parcours=LO->suivant;
+        int res_Admet_Unique;
+        int colonne;
+        
 
-	int a_modifie =1;
-	while (parcours!=parcours->suivant && a_modifie==1)
-	{
-		printf("Boucle\n");
-		a_modifie=0;
-		do
-		{
-			res_Admet_Unique=Admet_Unique(LC[parcours->courant.ligne][parcours->courant.colonne]);	
-			if (res_Admet_Unique!=0)
-			{
-				Fermer_Case(res_Admet_Unique,grille,parcours->courant,LC);
-				enleve_elt_case(&LO,parcours->courant);
-				grille[parcours->courant.ligne][parcours->courant.colonne] = res_Admet_Unique;
-				a_modifie=1;
-			}
-		
-			parcours=parcours->suivant;
+        int a_modifie =1;
+        while (parcours!=parcours->suivant && a_modifie==1)
+        {
+                printf("Boucle\n");
+                a_modifie=0;
+                do
+                {
+                        res_Admet_Unique=Admet_Unique(LC[parcours->courant.ligne][parcours->courant.colonne]);  
+                        if (res_Admet_Unique!=0)
+                        {
+                                Fermer_Case(res_Admet_Unique,grille,parcours->courant,LC);
+                                enleve_elt_case(&LO,parcours->courant);
+                                grille[parcours->courant.ligne][parcours->courant.colonne] = res_Admet_Unique;
+                                a_modifie=1;
+                        }
+                
+                        parcours=parcours->suivant;
 
-			if (parcours==LO)parcours = parcours->suivant;
-			
-		}while (parcours!=parcours->suivant && parcours!=LO->suivant);
-	}
-	
-	if (a_modifie==0)return 0;
-	else return 1;
+                        if (parcours==LO)parcours = parcours->suivant;
+                        
+                }while (parcours!=parcours->suivant && parcours!=LO->suivant);
+        }
+        
+        if (a_modifie==0)return 0;
+        else return 1;
 }
 
 int nbr_solution = 0;
@@ -51,82 +50,61 @@ int solution[9][9];
 
 int Backtrack(int grille[9][9], L_Cases LO, L_Candidats LC[9][9])
 {
-	L_Cases tmp = LO->suivant;
-	clock_t a,b;
-	double c;
-	a = clock();
-	resoud(grille, LO, tmp, LC);
-	b = clock();
-	c = (double)(b-a)/CLOCKS_PER_SEC;
-	Affiche_Grille(solution);
-	printf("Temps écoulé :%2.5f\n", c);
-	return nbr_solution;
+        L_Cases tmp = LO->suivant;
+        clock_t a,b;
+        double c;
+        a = clock();
+        resoud(grille, LO, tmp, LC);
+        b = clock();
+        c = (double)(b-a)/CLOCKS_PER_SEC;
+        Affiche_Grille(solution);
+        printf("Temps écoulé :%2.5f\n", c);
+        return nbr_solution;
 
 }
 
 resoud(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9])
 {
-	if(L == origine) {
-		nbr_solution++;
-		if(nbr_solution == 1)
-		{
-			memcpy(solution, grille, sizeof solution);
-		}
-		return;
-	}else{
-		L_Candidats c = LC[L->courant.ligne][L->courant.colonne];
-		int res;
-		int valeur_courant;
-		do{
-			if(c == NULL) return ;
+        if(L == origine) {
+                nbr_solution++;
+                if(nbr_solution == 1)
+                {
+                        memcpy(solution, grille, sizeof solution);
+                }
+                return;
+        }else{
+                L_Candidats c = LC[L->courant.ligne][L->courant.colonne];
+                int res;
+                int valeur_courant;
+                do{
+                        if(c == NULL) return ;
 
-			valeur_courant = c->courant;
-			Fermer_Case(c->courant,grille, L->courant, LC);
-			resoud(grille,origine, (L->suivant), LC);
+                        valeur_courant = c->courant;
+                        Fermer_Case(c->courant,grille, L->courant, LC);
+                        resoud(grille,origine, (L->suivant), LC);
 
-			Ouvrir_Case(valeur_courant,grille,L->courant, LC,origine);
-			
-			c = LC[L->courant.ligne][L->courant.colonne]; 
-			while(c->suivant && c->courant != valeur_courant) 
-			{
+                        Ouvrir_Case(valeur_courant,grille,L->courant, LC,origine);
+                        
+                        c = LC[L->courant.ligne][L->courant.colonne]; 
+                        while(c->suivant && c->courant != valeur_courant) 
+                        {
+                                c = c->suivant;
+                        }
+                        c = c->suivant;
+                }while(c);
 
-				Ouvrir_Case(valeur_courant,grille,L->courant, LC,origine);
-				
-				c = LC[L->courant.ligne][L->courant.colonne]; 
-				while(c->suivant && c->courant != valeur_courant) 
-				{
-					c = c->suivant;
-				}
-// 				for(colonne = 0; colonne < 9; colonne++)
-// 				{
-// 					printf("###Affichage de la ligne %d colonne %d\n", l, colonne);
-// 					affiche_liste_Candidats(LC[l][colonne]);
-// 				}
-// 				printf("---------------------------------------------------------------------");
-// 				printf("---------------------------------------------------------------------");
-// 				printf("%d %d\n",valeur_courant, c->courant);
-
-				c = c->suivant;
-			}else{
-					//Fermer_Case
-// 					printf("€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€€e€");
-					return res;
-			}
-		}while(c);
-// 		printf("Retour fin \n");
-		return 0;
-	}
+        }
 }
 
 
 
 // int main(){
-// 	printf("Bienvenue !!!\n");
-// 	int Grille[9][9]={{0}};
-// 	L_Candidats  LC[9][9]= {{NULL}};
-// 	L_Cases LO = creer_liste_vide();
-// 	char lien[] = "../res/g1.txt";
+//      printf("Bienvenue !!!\n");
+//      int Grille[9][9]={{0}};
+//      L_Candidats  LC[9][9]= {{NULL}};
+//      L_Cases LO = creer_liste_vide();
+//      char lien[] = "../res/g1.txt";
 // 
-// 	Init_Data(LC, LO, Grille, lien);
-// 	Affiche_Grille(Grille);
+//      Init_Data(LC, LO, Grille, lien);
+//      Affiche_Grille(Grille);
 // }
