9514865c726b7daf3731d13233d7bcb43072055c
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-06-12 10:23:33 +0200

    ajout de la fonction 'resoud' dans le header 'methodeResolution.h'

--- src/methodeResolution.h
+++ src/methodeResolution.h
@@ -38,6 +38,6 @@ int Backtrack(int grille[9][9], L_Cases LO, L_Candidats LC[9][9]);
  * 	@param L, Liste des cases ouvertes sur l'element suivant
  * 	@param LC, Liste des cases ouvertes
  */
-void resoud(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9])
+void  resoud(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9]);
 
 #endif
