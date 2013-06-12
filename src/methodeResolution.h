138f91245c9d3d291fec0b0aa906ec1b7aa9b31b
nicoptere <11332726+nicoptere2@users.noreply.github.com>
2013-06-12 10:21:30 +0200

    modification du comptage du temps...

--- src/methodeResolution.h
+++ src/methodeResolution.h
@@ -31,4 +31,13 @@ int fermerGrille(int grille[9][9], L_Cases LO, L_Candidats LC[9][9]);
  */
 int Backtrack(int grille[9][9], L_Cases LO, L_Candidats LC[9][9]);
 
+/**
+ * 	Fonction de resolution de la methode de backtracking
+ * 	@param grille, grille du sudoku
+ * 	@param origine, Liste des cases ouvertes au debut de l'appel de la fonctions
+ * 	@param L, Liste des cases ouvertes sur l'element suivant
+ * 	@param LC, Liste des cases ouvertes
+ */
+void resoud(int grille[9][9], L_Cases origine, L_Cases L, L_Candidats LC[9][9])
+
 #endif
